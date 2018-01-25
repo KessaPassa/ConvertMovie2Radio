3710fe8e5c990417b15893f698ac55b6ca353796
KessaPassa <k.someya.gyao@gmail.com>
2018-01-25 12:48:55 +0900

    tried

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request, url_for, abort, Response
 import os
 import main
@@ -10,8 +10,8 @@ port = int(os.getenv('VCAP_APP_PORT', 8080))
 
 @app.route("/api", methods=['GET'])
 def index():
-    url = request.args.get("url")
-    main.start(url)
+    # url = request.args.get("url")
+    # main.start(url)
 
     response = jsonify({'message': "File is uploaded GoogleDrive"})
     response.status_code = 200
