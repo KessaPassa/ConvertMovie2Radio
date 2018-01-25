b55dbe7f2132542873eab7ce3051f5e795590251
KessaPassa <k.someya.gyao@gmail.com>
2018-01-25 12:26:00 +0900

    herokuに上げるように修正

--- DownloadFromYoutube.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-def download():
-    url = input("URL>")
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
-    video.download()
-    print(yt.title, "のダウンロード完了")
-    return yt.title
--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def index():
     url = request.args.get("url")
     main.start(url)
 
-    response = jsonify({'message': "GoogleDriveにアップロードされました"})
+    response = jsonify({'message': "File is uploaded GoogleDrive"})
     response.status_code = 200
     return response
 
