7aa75ae05f85b2cc0b8e8f1a692306af742db03f
KessaPassa <k.someya.gyao@gmail.com>
2019-04-16 16:46:36 +0900

    seleniumにretryを追加

--- src/googledrive.py
+++ src/googledrive.py
@@ -29,7 +29,7 @@ CLIENT_SECRET = 'client_secret'
 REDIRECT_URI = 'redirect_uri'
 
 # 最大待機時間（秒）
-wait_time = 10
+wait_time = 20
 
 
 def setup():
@@ -67,55 +67,63 @@ def setup():
 
 
 def get_authrozation_code(driver):
-    login_id = os.getenv('login_id') or os.environ.get('login_id')
-    login_password = os.getenv('login_password') or os.environ.get('login_password')
+    for i in range(0, 3):
+        try:
+            login_id = os.getenv('login_id') or os.environ.get('login_id')
+            login_password = os.getenv('login_password') or os.environ.get('login_password')
 
-    # IDを入力
-    login_id_xpath = '//*[@id="identifierNext"]'
-    WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_id_xpath)))
-    driver.find_element_by_name("identifier").send_keys(login_id)
-    driver.find_element_by_xpath(login_id_xpath).click()
-    time.sleep(2)
+            # IDを入力
+            login_id_xpath = '//*[@id="identifierNext"]'
+            WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_id_xpath)))
+            driver.find_element_by_name("identifier").send_keys(login_id)
+            driver.find_element_by_xpath(login_id_xpath).click()
+            time.sleep(2)
 
-    # パスワードを入力
-    login_password_xpath = '//*[@id="passwordNext"]'
-    WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_password_xpath)))
-    driver.find_element_by_name("password").send_keys(login_password)
-    driver.find_element_by_xpath(login_password_xpath).click()
+            # パスワードを入力
+            login_password_xpath = '//*[@id="passwordNext"]'
+            WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_password_xpath)))
+            driver.find_element_by_name("password").send_keys(login_password)
+            driver.find_element_by_xpath(login_password_xpath).click()
 
-    # アカウント選択
-    # select_account = '//*[@data-identifier="{}"]'.format(login_id)
-    # # select_account = '//*[@jsname="rwl3qc"]'
-    # WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, select_account)))
-    # driver.find_element_by_xpath(select_account).submit()
-    # time.sleep(1)
+            # アカウント選択
+            # select_account = '//*[@data-identifier="{}"]'.format(login_id)
+            # # select_account = '//*[@jsname="rwl3qc"]'
+            # WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, select_account)))
+            # driver.find_element_by_xpath(select_account).submit()
+            # time.sleep(1)
 
-    # ダイアログ取得
-    # dialog_xpath = '//*[@class="g3VIld aQ7q2c Up8vH J9Nfi iWO5td"][@id="oauthScopeDialog"]'
-    # WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, dialog_xpath)))
-    # dialog = driver.find_element_by_xpath(dialog_xpath)
+            # ダイアログ取得
+            # dialog_xpath = '//*[@class="g3VIld aQ7q2c Up8vH J9Nfi iWO5td"][@id="oauthScopeDialog"]'
+            # WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, dialog_xpath)))
+            # dialog = driver.find_element_by_xpath(dialog_xpath)
 
-    # oauthScopeDialogの許可
-    allow_login_xpath = '//*[@class="U26fgb O0WRkf oG5Srb C0oVfc kHssdc M9Bg4d"]'
-    WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, allow_login_xpath)))
-    allow_login = driver.find_element_by_xpath(allow_login_xpath)
-    allow_login.click()
+            # oauthScopeDialogの許可
+            allow_login_xpath = '//*[@class="U26fgb O0WRkf oG5Srb C0oVfc kHssdc M9Bg4d"]'
+            WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, allow_login_xpath)))
+            allow_login = driver.find_element_by_xpath(allow_login_xpath)
+            allow_login.click()
 
-    # スコープの許可
-    approve_access_xpath = '//*[@id="submit_approve_access"]'
-    WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, approve_access_xpath)))
-    approve_access = driver.find_element_by_xpath(approve_access_xpath)
-    approve_access.click()
+            # スコープの許可
+            approve_access_xpath = '//*[@id="submit_approve_access"]'
+            WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, approve_access_xpath)))
+            approve_access = driver.find_element_by_xpath(approve_access_xpath)
+            approve_access.click()
 
-    # authorization_codeの取得
-    authorization_xpath = '//textarea[@class="qBHUIf"]'
-    WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, authorization_xpath)))
-    authorization = driver.find_element_by_xpath(authorization_xpath)
-    code = authorization.text
+            # authorization_codeの取得
+            authorization_xpath = '//textarea[@class="qBHUIf"]'
+            WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, authorization_xpath)))
+            authorization = driver.find_element_by_xpath(authorization_xpath)
+            code = authorization.text
 
-    driver.quit()
+            driver.quit()
+            return code
 
-    return code
+        except Exception as e:
+            print('エラー')
+            print(e)
+            continue
+
+    print('だめ')
 
 
 def get_tokens(code):
